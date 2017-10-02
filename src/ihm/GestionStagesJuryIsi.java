4329bf836b0abbc3323d54dfd07e8325d1737765
lucasnoga <lucasnoga@192.168.1.46>
2017-10-02 10:05:17 +0200

    Ajout de la methode addListener pour l'ensemble des Listener

--- src/ihm/GestionStagesJuryIsi.java
+++ src/ihm/GestionStagesJuryIsi.java
@@ -40,6 +40,7 @@ public class GestionStagesJuryIsi {
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JTextField sourcePDF;
 	private JRadioButton isi1, isi2, isi3;
+	JButton exit, findPDF conversionTxt_Csv, conversionPdf_Txt
 
 	/**
 	 * conversion d'un PDF en une chaine de caract�res
@@ -82,6 +83,7 @@ public class GestionStagesJuryIsi {
 	 */
 	public GestionStagesJuryIsi() {
 		initialize();
+		addListener();
 	}
 
 	/**
@@ -112,9 +114,8 @@ public class GestionStagesJuryIsi {
 		frmGestionStagesJury.getContentPane().add(cibleCSV);
 		cibleCSV.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Niveau ISI");
-		lblNewLabel_2.setBounds(10, 153, 88, 14);
-		frmGestionStagesJury.getContentPane().add(lblNewLabel_2);
+		new JLabel("Niveau ISI").setBounds(10, 153, 88, 14);
+		frmGestionStagesJury.getContentPane().add(new JLabel("Niveau ISI"));
 		
 		isi1 = new JRadioButton("Isi 1");
 		isi1.setSelected(true);
@@ -132,9 +133,63 @@ public class GestionStagesJuryIsi {
 		isi3.setBounds(276, 149, 109, 23);
 		frmGestionStagesJury.getContentPane().add(isi3);
 		
-		// Bouton lan�ant la conversion TXT --> "Filtrage" --> CSV 
-		JButton ConversionBouton = new JButton("Conversion  TXT -> CSV");
-		ConversionBouton.addMouseListener(new MouseAdapter() {
+		// Bouton lançant la conversion TXT --> "Filtrage" --> CSV 
+		conversionTxt_Csv = new JButton("Conversion  TXT -> CSV");
+		conversionButtonTxtCsv.setBounds(296, 79, 176, 23);
+		frmGestionStagesJury.getContentPane().add(conversionButtonTxtCsv);
+		
+		// Bouton pour quitter l'application
+		exit = new JButton("Quitter");
+		exit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.exit(0);
+			}
+		});
+		exit.setBounds(664, 169, 109, 23);
+		frmGestionStagesJury.getContentPane().add(exit);
+		
+		// Bouton pour le chargement du fichier PDF 
+		findPDF = new JButton("...");
+		findPDF.setFont(new Font("Tahoma", Font.BOLD, 11));
+		findPDF.setBounds(740, 7, 33, 23);
+		frmGestionStagesJury.getContentPane().add(findPDF);
+		
+		JLabel lblNewLabel_3 = new JLabel("Source PDF");
+		lblNewLabel_3.setBounds(10, 11, 77, 14);
+		frmGestionStagesJury.getContentPane().add(lblNewLabel_3);
+		
+		sourcePDF = new JTextField();
+		sourcePDF.setBounds(108, 8, 627, 20);
+		frmGestionStagesJury.getContentPane().add(sourcePDF);
+		sourcePDF.setColumns(10);
+		
+		// Bouton de convertion PDF --> TXT 
+		conversionPdf_Txt = new JButton("Conversion  PDF >- TXT");
+		conversionPdf_Txt.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (!(sourcePDF.getText()=="") && !(sourceTXT.getText()==""))
+					try {
+						maConversionPDFtoText (sourcePDF.getText(), sourceTXT.getText());
+						System.out.println("Conversion PDF --> TXT termin�e");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+			}
+		});
+		conversionPdf_Txt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		conversionPdf_Txt.setBounds(296, 28, 176, 23);
+		frmGestionStagesJury.getContentPane().add(conversionPdf_Txt);
+	}
+	
+	/** Methode qui ajoute les listeners aux boutons*/
+	private void addListener(){
+		conversionButtonTxtCsv.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				System.out.println(isi1.isSelected());/*TODO a enlever*/
@@ -147,22 +202,8 @@ public class GestionStagesJuryIsi {
 				System.out.println("Conversion TXT --> CSV termin�e");
 			}
 		});
-		ConversionBouton.setBounds(296, 79, 176, 23);
-		frmGestionStagesJury.getContentPane().add(ConversionBouton);
 		
-		// Bouton pour quitter l'application
-		JButton btnNewButton_1 = new JButton("Quitter");
-		btnNewButton_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				System.exit(0);
-			}
-		});
-		btnNewButton_1.setBounds(664, 169, 109, 23);
-		frmGestionStagesJury.getContentPane().add(btnNewButton_1);
 		
-		// Bouton pour le chargement du fichier PDF 
-		JButton findPDF = new JButton("...");
 		findPDF.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -185,40 +226,7 @@ public class GestionStagesJuryIsi {
 			      }
 			}
 		});
-		findPDF.setFont(new Font("Tahoma", Font.BOLD, 11));
-		findPDF.setBounds(740, 7, 33, 23);
-		frmGestionStagesJury.getContentPane().add(findPDF);
 		
-		JLabel lblNewLabel_3 = new JLabel("Source PDF");
-		lblNewLabel_3.setBounds(10, 11, 77, 14);
-		frmGestionStagesJury.getContentPane().add(lblNewLabel_3);
-		
-		sourcePDF = new JTextField();
-		sourcePDF.setBounds(108, 8, 627, 20);
-		frmGestionStagesJury.getContentPane().add(sourcePDF);
-		sourcePDF.setColumns(10);
-		
-		// Bouton de convertion PDF --> TXT 
-		JButton ConvertirEnTXT = new JButton("Conversion  PDF >- TXT");
-		ConvertirEnTXT.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if (!(sourcePDF.getText()=="") && !(sourceTXT.getText()==""))
-					try {
-						maConversionPDFtoText (sourcePDF.getText(), sourceTXT.getText());
-						System.out.println("Conversion PDF --> TXT termin�e");
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-			}
-		});
-		ConvertirEnTXT.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		ConvertirEnTXT.setBounds(296, 28, 176, 23);
-		frmGestionStagesJury.getContentPane().add(ConvertirEnTXT);
 	}
 
 
