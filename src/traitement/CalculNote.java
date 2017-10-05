c219971154ccb1c94beb52838d2300fcc8157d58
lucasnoga <lucasnoga@192.168.1.46>
2017-10-05 22:58:15 +0200

    Ajout de la classe CalculNote

---
+++ src/traitement/CalculNote.java
@@ -0,0 +1,22 @@
+package traitement;
+
+import java.util.HashMap;
+
+/**Classe permettant de faire les calculs sur les notes des etudiants*/
+public class CalculNote{
+
+	
+	
+	/** retourne un tableau associatif avec pour cle la note et comme valeur le nombre de fois la note obtenu dans le semestre
+	 * @return */
+	public HashMap<Note, Integer> calculsemestre(){
+		return null;	
+	}
+	
+	/**Permet de calculer les notes sur l'ensemble du semestre*/
+	/** retourne un tableau associatif avec pour cle la note et comme valeur le nombre de fois la note obtenu dans le cursus*/
+	public HashMap<Note, Integer> calculCursus(){
+		return null;
+	}
+
+}
